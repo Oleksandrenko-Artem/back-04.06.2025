9e43e2dfc44a7c99959ee47c8c4437e13f1ebde1
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-06-05 17:38:36 +0300

    Use readFile with promises

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-// const math = require('./math');
-// const message = require('./message');
-// const grades = require('./grades');
-// const countdown = require('./countdown');
-// const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
-// console.log('hi!');
-
-// const number1 = 8;
-// const number2 = 27;
-
-// console.log(math.add(number1, number2));
-// console.log(message.sayHello());
-// console.log(math);
-
-// const myGrades = [9, 7, 12];
-// console.log(grades.calcAverage(myGrades).toFixed(3));
-
-// countdown.startCountDown(10);
-
-// console.log(__filename);
-// const pathToTextFile = path.resolve(__dirname, 'assets', 'text.txt');
-// console.log(pathToTextFile);
-
-// console.log(fs);
-fs.readFile('./assets/text.txt', 'utf8', (error, data) => {
-    if (error) {
+async function readTextFile(pathToTextFile) {
+    try {
+        const data = await fs.readFile(pathToTextFile, 'utf8');
+        console.log(data);
+    } catch (error) {
         console.log(error);
-        return;
     }
-    console.log(data);
-});
+}
+readTextFile('./assets/text.txt');
